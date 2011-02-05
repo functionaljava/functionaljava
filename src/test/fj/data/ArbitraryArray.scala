4e5a6576b77658c31838609a1ed5d25e5083f88a
retronym <jzaugg@gmail.com>
2011-02-05 14:06:17 +0100

    Avoid ClassCastException from http://lampsvn.epfl.ch/trac/scala/ticket/4216

--- src/test/fj/data/ArbitraryArray.scala
+++ src/test/fj/data/ArbitraryArray.scala
@@ -6,6 +6,6 @@ import org.scalacheck.Arbitrary.arbitrary
 import Array.array
 
 object ArbitraryArray {
-  implicit def arbitraryArray[A: Manifest : Arbitrary]: Arbitrary[Array[A]] =
-    Arbitrary(arbitrary[scala.Array[A]].map(array(_: _*)))
+  implicit def arbitraryArray[A : Arbitrary]: Arbitrary[Array[A]] =
+    Arbitrary(arbitrary[scala.List[A]].map(array(_: _*)))
 }
