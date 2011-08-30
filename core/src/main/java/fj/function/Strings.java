ee4268bcb12931f21fef240ca306fd8992c8ce7a
Martin Grotzke <martin.grotzke@googlemail.com>
2011-08-30 22:26:22 +0200

    Add isNullOrEmpty, isNullOrBlank and isNotNullOrBlank to Strings.

--- core/src/main/java/fj/function/Strings.java
+++ core/src/main/java/fj/function/Strings.java
@@ -2,7 +2,10 @@ package fj.function;
 
 import fj.F;
 import fj.F2;
+import fj.data.Stream;
 import static fj.Function.curry;
+import static fj.function.Booleans.not;
+import static fj.function.Characters.isWhitespace;
 
 /**
  * Curried string functions.
@@ -15,12 +18,57 @@ public final class Strings {
   }
 
   /**
+   * This function checks if a given String contains any non-whitespace character
+   * (according to {@link Character#isWhitespace(char)}) and if it's also not
+   * <code>null</code> and not empty ("").
+   * 
+   * @see #isNullOrBlank
+   * @see Character#isWhitespace(char)
+   * @see Characters#isWhitespace
+   */
+  public static final F<String, Boolean> isNotNullOrBlank = new F<String, Boolean>() {
+    @Override
+    public Boolean f(final String a) {
+      return isNotNullOrEmpty.f(a) && Stream.fromString(a).find(not(isWhitespace)).isSome();
+    }
+  };
+
+  /**
+   * This function checks if a given String is whitespace (according to {@link Character#isWhitespace(char)}),
+   * empty ("") or <code>null</code>.
+   * 
+   * @see #isNotNullOrBlank
+   * @see Character#isWhitespace(char)
+   * @see Characters#isWhitespace
+   */
+  public static final F<String, Boolean> isNullOrBlank = new F<String, Boolean>() {
+    @Override
+    public Boolean f(final String a) {
+      return isNullOrEmpty.f(a) || Stream.fromString(a).find(not(isWhitespace)).isNone();
+    }
+  };
+
+  /**
    * This function checks if a given String is neither <code>null</code> nor empty.
+   * 
+   * @see #isNullOrEmpty
    */
   public static final F<String, Boolean> isNotNullOrEmpty = new F<String, Boolean>() {
     @Override
     public Boolean f(final String a) {
       return a != null && a.length() > 0;
+    }
+  };
+
+  /**
+   * This function checks if a given String is <code>null</code> or empty ({@link String#isEmpty()}).
+   * 
+   * @see #isNotNullOrEmpty
+   */
+  public static final F<String, Boolean> isNullOrEmpty = new F<String, Boolean>() {
+    @Override
+    public Boolean f(final String a) {
+      return a == null || a.length() == 0;
     }
   };
 
