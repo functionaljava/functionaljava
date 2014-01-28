6f4e858cb65089216162667bb8d8fe2efc6b94d9
Shimi Bandiel <shimi@trainologic.com>
2014-01-28 10:50:47 +0200

    -> j8

--- core/src/main/java/fj/function/Characters.java
+++ core/src/main/java/fj/function/Characters.java
@@ -18,84 +18,26 @@ public final class Characters {
 	public static final F<Character, Boolean> isUpperCase = Character::isUpperCase;
 	public static final F<Character, Boolean> isTitleCase = Character::isTitleCase;
 	public static final F<Character, Boolean> isDigit = Character::isDigit;
-	public static final F<Character, Boolean> isDefined = new F<Character, Boolean>() {
-		public Boolean f(final Character ch) {
-			return Character.isDefined(ch);
-		}
-	};
-	public static final F<Character, Boolean> isLetter = new F<Character, Boolean>() {
-		public Boolean f(final Character ch) {
-			return Character.isLetter(ch);
-		}
-	};
-	public static final F<Character, Boolean> isLetterOrDigit = new F<Character, Boolean>() {
-		public Boolean f(final Character ch) {
-			return Character.isLetterOrDigit(ch);
-		}
-	};
-	public static final F<Character, Boolean> isJavaIdentifierStart = new F<Character, Boolean>() {
-		public Boolean f(final Character ch) {
-			return Character.isJavaIdentifierStart(ch);
-		}
-	};
-	public static final F<Character, Boolean> isJavaIdentifierPart = new F<Character, Boolean>() {
-		public Boolean f(final Character ch) {
-			return Character.isJavaIdentifierPart(ch);
-		}
-	};
+	public static final F<Character, Boolean> isDefined = Character::isDefined;
+	public static final F<Character, Boolean> isLetter = Character::isLetter;
+	public static final F<Character, Boolean> isLetterOrDigit = Character::isLetterOrDigit;
+	public static final F<Character, Boolean> isJavaIdentifierStart = Character::isJavaIdentifierStart;
+	public static final F<Character, Boolean> isJavaIdentifierPart = Character::isJavaIdentifierPart;
 	public static final F<Character, Boolean> isUnicodeIdentifierStart = Character::isUnicodeIdentifierStart;
-
 	public static final F<Character, Boolean> isUnicodeIdentifierPart = Character::isUnicodeIdentifierPart;
-
 	public static final F<Character, Boolean> isIdentifierIgnorable = Character::isIdentifierIgnorable;
 	public static final F<Character, Character> toLowerCase = Character::toLowerCase;
 	public static final F<Character, Character> toUpperCase = Character::toUpperCase;
 	public static final F<Character, Character> toTitleCase = Character::toTitleCase;
 	public static final F<Character, F<Integer, Integer>> digit = curry((ch,
 			radix) -> Character.digit(ch, radix));
-	public static final F<Character, Integer> getNumericValue = new F<Character, Integer>() {
-		public Integer f(final Character ch) {
-			return Character.getNumericValue(ch);
-		}
-	};
-	public static final F<Character, Boolean> isSpaceChar = new F<Character, Boolean>() {
-		public Boolean f(final Character ch) {
-			return Character.isSpaceChar(ch);
-		}
-	};
-	public static final F<Character, Boolean> isWhitespace = new F<Character, Boolean>() {
-		public Boolean f(final Character ch) {
-			return Character.isWhitespace(ch);
-		}
-	};
-	public static final F<Character, Boolean> isISOControl = new F<Character, Boolean>() {
-		public Boolean f(final Character ch) {
-			return Character.isISOControl(ch);
-		}
-	};
-	public static final F<Character, Integer> getType = new F<Character, Integer>() {
-		public Integer f(final Character ch) {
-			return Character.getType(ch);
-		}
-	};
-	public static final F<Character, Byte> getDirectionality = new F<Character, Byte>() {
-		public Byte f(final Character ch) {
-			return Character.getDirectionality(ch);
-		}
-	};
-	public static final F<Character, Boolean> isMirrored = new F<Character, Boolean>() {
-		public Boolean f(final Character ch) {
-			return Character.isMirrored(ch);
-		}
-	};
-	public static final F<Character, Character> reverseBytes = new F<Character, Character>() {
-		public Character f(final Character ch) {
-			return Character.reverseBytes(ch);
-		}
-	};
-	public static final F<Character, Boolean> isNewLine = new F<Character, Boolean>() {
-		public Boolean f(final Character c) {
-			return c == '\n';
-		}
-	};
+	public static final F<Character, Integer> getNumericValue = Character::getNumericValue;
+	public static final F<Character, Boolean> isSpaceChar = Character::isSpaceChar;
+	public static final F<Character, Boolean> isWhitespace = Character::isWhitespace;
+	public static final F<Character, Boolean> isISOControl = Character::isISOControl;
+	public static final F<Character, Integer> getType = Character::getType;
+	public static final F<Character, Byte> getDirectionality = Character::getDirectionality;
+	public static final F<Character, Boolean> isMirrored = Character::isMirrored;
+	public static final F<Character, Character> reverseBytes = Character::reverseBytes;
+	public static final F<Character, Boolean> isNewLine = c ->c == '\n';
 }
