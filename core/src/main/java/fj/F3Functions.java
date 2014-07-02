28f099f867b81e5490e25b2910912cce751e46db
Mark Perry <mperry@onetest.com.au>
2014-07-02 16:24:17 +1000

    Partial application for F3 and F4

--- core/src/main/java/fj/F3Functions.java
+++ core/src/main/java/fj/F3Functions.java
@@ -26,4 +26,14 @@ public class F3Functions {
         };
     }
 
+    /**
+     * Partial application.
+     *
+     * @param a The <code>A</code> to which to apply this function.
+     * @return The function partially applied to the given argument.
+     */
+    static public <A, B, C, D> F2<B, C, D> f(final F3<A, B, C, D> f, final A a) {
+        return (b, c) -> f.f(a, b, c);
+    }
+
 }
--- core/src/main/java/fj/F4Functions.java
+++ core/src/main/java/fj/F4Functions.java
@@ -24,4 +24,14 @@ public class F4Functions {
         };
     }
 
+    /**
+     * Partial application.
+     *
+     * @param a The <code>A</code> to which to apply this function.
+     * @return The function partially applied to the given argument.
+     */
+    static public <A, B, C, D, E> F3<B, C, D, E> f(final F4<A, B, C, D, E> f, final A a) {
+        return (b, c, d) -> f.f(a, b, c, d);
+    }
+
 }
