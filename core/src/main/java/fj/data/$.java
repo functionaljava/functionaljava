c0033d20a5c2f8ce063b4b893a319cc807e4f23b
Mark Perry <maperry78@yahoo.com.au>
2014-07-14 00:58:04 +1000

    #53: git status

--- core/src/main/java/fj/data/$.java
+++ core/src/main/java/fj/data/$.java
@@ -7,7 +7,6 @@ import fj.P1;
  * is the identity morphism from B to B.
  */
 @SuppressWarnings({"UnusedDeclaration"})
-@Deprecated
 public final class $<A, B> implements P1<B> {
 
   private final B b;
@@ -16,11 +15,24 @@ public final class $<A, B> implements P1<B> {
     this.b = b;
   }
 
+	/**
+	 * @deprecated  JDK 8 warns '_' may not be supported after SE 8.  Replaced by {@link #constant} and synonym {@link #__} (prefer constant).
+	 */
+	@Deprecated
   public static <A, B> $<A, B> _(final B b) {
-    return new $<A, B>(b);
+    return constant(b);
   }
 
-  public B _1() {
+	public static <A, B> $<A, B> __(final B b) {
+		return constant(b);
+	}
+
+	public static <A, B> $<A, B> constant(final B b) {
+		return new $<A, B>(b);
+	}
+
+
+	public B _1() {
     return b;
   }
 }
