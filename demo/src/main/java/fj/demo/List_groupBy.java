fc9fe00e55b3b9607f87372da1caf67080a28b73
Martin Seeler <mseeler@avantgarde-labs.de>
2014-12-04 22:51:56 +0100

    Add demo for groupBy

---
+++ demo/src/main/java/fj/demo/List_groupBy.java
@@ -0,0 +1,47 @@
+package fj.demo;
+
+import fj.Function;
+import fj.data.HashMap;
+import fj.data.List;
+
+import static fj.data.List.list;
+
+public final class List_groupBy {
+
+  public static void main(final String... args) {
+    keyDemo();
+    keyValueDemo();
+    keyValueAccDemo();
+  }
+
+  private static void keyDemo() {
+    System.out.println("KeyDemo");
+    final List<String> words = list("Hello", "World", "how", "are", "your", "doing");
+    final HashMap<Integer, List<String>> lengthMap = words.groupBy(String::length);
+
+    lengthMap.foreach(entry -> {
+      System.out.println(String.format("Words with %d chars: %s", entry._1(), entry._2()));
+    });
+  }
+
+  private static void keyValueDemo() {
+    System.out.println("KeyValueDemo");
+    final List<Integer> xs = list(1, 2, 3, 4, 5, 6, 7, 8, 9);
+    final HashMap<Integer, List<String>> result = xs.groupBy(x -> x % 3, Integer::toBinaryString);
+
+    result.foreach(entry -> {
+      System.out.println(String.format("Numbers with reminder %d are %s", entry._1(), entry._2()));
+    });
+  }
+
+  private static void keyValueAccDemo() {
+    System.out.println("KeyValueAccDemo");
+    final List<String> words = list("Hello", "World", "how", "are", "your", "doing");
+    final HashMap<Integer, Integer> lengthCounts =
+        words.groupBy(String::length, Function.identity(), 0, (word, sum) -> sum + 1);
+
+    lengthCounts.foreach(entry -> {
+      System.out.println(String.format("Words with %d chars: %s", entry._1(), entry._2()));
+    });
+  }
+}
