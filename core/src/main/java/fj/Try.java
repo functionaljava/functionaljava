8f2a793b4ca298ef3a6899c25df6dba8f1e9b7f3
amarpotghan <amarpotghan@gmail.com>
2014-12-21 14:05:36 +0530

    Refactoring Try type

--- core/src/main/java/fj/Try.java
+++ core/src/main/java/fj/Try.java
@@ -24,9 +24,9 @@ public class Try {
     static public <A, E extends Exception> P1<Validation<E, A>> f(final Try0<A, E> t) {
         return P.lazy(u -> {
             try {
-                return Validation.success(t.f());
+                return success(t.f());
             } catch (Exception e) {
-                return Validation.fail((E) e);
+                return fail((E) e);
             }
         });
     }
@@ -42,7 +42,7 @@ public class Try {
             try {
                 return Validation.success(t.f(a));
             } catch (Exception e) {
-                return Validation.fail((E) e);
+                return fail((E) e);
             }
         };
     }
@@ -56,9 +56,9 @@ public class Try {
     static public <A, B, C, E extends Exception> F2<A, B, Validation<E, C>> f(final Try2<A, B, C, E> t) {
         return (a, b) -> {
             try {
-                return Validation.success(t.f(a, b));
+                return success(t.f(a, b));
             } catch (Exception e) {
-                return Validation.fail((E) e);
+                return fail((E) e);
             }
         };
     }
@@ -88,9 +88,9 @@ public class Try {
     static public <A, B, C, D, E, Z extends Exception> F4<A, B, C, D, Validation<Z, E>> f(final Try4<A, B, C, D, E, Z> t) {
         return (a, b, c, d) -> {
             try {
-                return Validation.success(t.f(a, b, c, d));
+                return success(t.f(a, b, c, d));
             } catch (Exception ex) {
-                return Validation.fail((Z) ex);
+                return fail((Z) ex);
             }
         };
     }
@@ -104,9 +104,9 @@ public class Try {
     static public <A, B, C, D, E, F, Z extends Exception> F5<A, B, C, D, E, Validation<Z, F>> f(final Try5<A, B, C, D, E, F, Z> t) {
         return (a, b, c, d, e) -> {
             try {
-                return Validation.success(t.f(a, b, c, d, e));
+                return success(t.f(a, b, c, d, e));
             } catch (Exception ex) {
-                return Validation.fail((Z) ex);
+                return fail((Z) ex);
             }
         };
     }
@@ -120,9 +120,9 @@ public class Try {
     static public <A, B, C, D, E, F, G, Z extends Exception> F6<A, B, C, D, E, F, Validation<Z, G>> f(final Try6<A, B, C, D, E, F, G, Z> t) {
         return (a, b, c, d, e, f) -> {
             try {
-                return Validation.success(t.f(a, b, c, d, e, f));
+                return success(t.f(a, b, c, d, e, f));
             } catch (Exception ex) {
-                return Validation.fail((Z) ex);
+                return fail((Z) ex);
             }
         };
     }
@@ -136,9 +136,9 @@ public class Try {
     static public <A, B, C, D, E, F, G, H, Z extends Exception> F7<A, B, C, D, E, F, G, Validation<Z, H>> f(final Try7<A, B, C, D, E, F, G, H, Z> t) {
         return (a, b, c, d, e, f, g) -> {
             try {
-                return Validation.success(t.f(a, b, c, d, e, f, g));
+                return success(t.f(a, b, c, d, e, f, g));
             } catch (Exception ex) {
-                return Validation.fail((Z) ex);
+                return fail((Z) ex);
             }
         };
     }
@@ -149,12 +149,12 @@ public class Try {
      * @param t A TryCatch8 to promote
      * @return A Validation with an Exception on the failure side and its result on the success side.
      */
-    static public <A, B, C, D, E, F, G, H, I, Z extends Exception> F8<A, B, C, D, E, F, G, H, Validation<Z, I>> f(final Try8<A, B, C, D, E, F, G, H, I, Z> t) {
+    public static <A, B, C, D, E, F, G, H, I, Z extends Exception> F8<A, B, C, D, E, F, G, H, Validation<Z, I>> f(final Try8<A, B, C, D, E, F, G, H, I, Z> t) {
         return (a, b, c, d, e, f, g, h) -> {
             try {
-                return Validation.success(t.f(a, b, c, d, e, f, g, h));
+                return success(t.f(a, b, c, d, e, f, g, h));
             } catch (Exception ex) {
-                return Validation.fail((Z) ex);
+                return fail((Z) ex);
             }
         };
     }
