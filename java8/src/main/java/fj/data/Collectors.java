09552ee6ce0e9af59bab4b1bbe2fb0d8c22c8da9
Paul Horn <knutwalker@gmail.com>
2014-12-24 03:22:05 +0100

    Add some Collectors for Java8

---
+++ java8/src/main/java/fj/data/Collectors.java
@@ -0,0 +1,36 @@
+package fj.data;
+
+import java.util.stream.Collector;
+
+public final class Collectors {
+
+  private Collectors() {
+  }
+
+  public static <A> Collector<A, List.Buffer<A>, List<A>> toList() {
+    return Collector.of(
+        List.Buffer::new,
+        List.Buffer::snoc,
+        (acc1, acc2) -> acc1.append(acc2.toList()),
+        List.Buffer::toList
+    );
+  }
+
+  public static <A> Collector<A, List.Buffer<A>, Array<A>> toArray() {
+    return Collector.of(
+        List.Buffer::new,
+        List.Buffer::snoc,
+        (acc1, acc2) -> acc1.append(acc2.toList()),
+        (buf) -> Array.iterableArray(buf.toList())
+    );
+  }
+
+  public static <A> Collector<A, List.Buffer<A>, Stream<A>> toStream() {
+    return Collector.of(
+        List.Buffer::new,
+        List.Buffer::snoc,
+        (acc1, acc2) -> acc1.append(acc2.toList()),
+        (buf) -> Stream.iterableStream(buf.toList())
+    );
+  }
+}
