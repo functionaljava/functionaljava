0645ad91db08567ffc96a4a1daba6dac9aefbc55
Mark Perry <maperry78@yahoo.com.au>
2015-01-11 21:49:48 +1000

    Issue #4: Fixed TreeMap.split and added splitLookup

--- core/src/test/java/fj/data/TreeMapTest.java
+++ core/src/test/java/fj/data/TreeMapTest.java
@@ -4,8 +4,12 @@ import fj.Equal;
 import fj.Ord;
 import fj.P3;
 import fj.Show;
+import fj.P;
 import org.junit.Assert;
 import org.junit.Test;
+
+import static fj.data.Option.some;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by MarkPerry on 11/01/2015.
@@ -14,28 +18,61 @@ public class TreeMapTest {
 
     @Test
     public void split() {
+        // do the split
         int pivot = 4;
-        List<Integer> l = List.range(1, 6);
+        int max = 5;
+        List<Integer> l = List.range(1, max + 1);
         TreeMap<Integer, String> m2 = TreeMap.map(Ord.intOrd, l.zip(l.map(i -> i.toString())));
-        P3<Set<String>, Option<String>, Set<String>> p = m2.split(pivot);
+        P3<Set<String>, Option<String>, Set<String>> p = m2.split(Ord.stringOrd, pivot);
+
+        // print debug info
         Show<TreeMap<Integer, String>> st = Show.treeMapShow(Show.intShow, Show.stringShow);
         Show<Set<String>> ss = Show.setShow(Show.stringShow);
         Show<Option<String>> so = Show.optionShow(Show.stringShow);
         Show<P3<Set<String>, Option<String>, Set<String>>> sp3 = Show.p3Show(ss, so, ss);
+        if (true) {
+            st.println(m2);
+            sp3.println(p);
+        }
 
-        st.println(m2);
-        sp3.println(p);
-
-        Equal<Set<String>> eq = Equal.setEqual(Equal.stringEqual);
-        Set<String> left = toSetString(List.list(1, 2, 3));
-        Set<String> right = toSetString(List.list(5));
-        Assert.assertTrue("Left side of split unexpected", eq.eq(left, p._1()));
-        Assert.assertTrue(eq.eq(right, p._1()));
-        Assert.assertTrue(Equal.optionEqual(Equal.stringEqual).eq(p._2(), Option.some(Integer.toString(pivot))));
+        // assert equals
+        Equal<Set<String>> seq = Equal.setEqual(Equal.stringEqual);
+        Set<String> left = toSetString(List.range(1, pivot));
+        Set<String> right = toSetString(List.range(pivot + 1, max + 1));
+        P3<Set<String>, Option<String>, Set<String>> expected = P.p(left, some(Integer.toString(pivot)), right);
+        assertTrue(Equal.p3Equal(seq, Equal.optionEqual(Equal.stringEqual), seq).eq(p, expected));
     }
 
     private static Set<String> toSetString(List<Integer> list) {
         return Set.set(Ord.stringOrd, list.map(i -> i.toString()));
+    }
+
+    @Test
+    public void splitLookup() {
+        // do the split
+        int pivot = 4;
+        int max = 5;
+        List<Integer> l = List.range(1, max + 1);
+        TreeMap<Integer, String> m2 = TreeMap.map(Ord.intOrd, l.zip(l.map(i -> i.toString())));
+        P3<TreeMap<Integer, String>, Option<String>, TreeMap<Integer, String>> p3 = m2.splitLookup(pivot);
+
+        // create expected output
+        List<Integer> leftList = List.range(1, pivot);
+        TreeMap<Integer, String> leftMap = TreeMap.map(Ord.intOrd, leftList.zip(leftList.map(i -> i.toString())));
+        List<Integer> rightList = List.range(pivot + 1, max + 1);
+        TreeMap<Integer, String> rightMap = TreeMap.map(Ord.intOrd, rightList.zip(rightList.map(i -> i.toString())));
+
+        // debug info
+        if (true) {
+            Show<TreeMap<Integer, String>> st = Show.treeMapShow(Show.intShow, Show.stringShow);
+            Show<P3<TreeMap<Integer, String>, Option<String>, TreeMap<Integer, String>>> sp3 = Show.p3Show(st, Show.optionShow(Show.stringShow), st);
+            sp3.println(p3);
+        }
+
+        // do the assert
+        Equal<TreeMap<Integer, String>> tme = Equal.treeMapEqual(Equal.intEqual, Equal.stringEqual);
+        Equal<P3<TreeMap<Integer, String>, Option<String>, TreeMap<Integer, String>>> eq = Equal.p3Equal(tme, Equal.optionEqual(Equal.stringEqual), tme);
+        assertTrue(eq.eq(p3, P.p(leftMap, some(Integer.toString(pivot)), rightMap)));
     }
 
 }
