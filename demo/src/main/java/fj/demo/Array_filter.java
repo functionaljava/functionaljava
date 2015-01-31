097b298a37956b9ccd0e45834938f1cf5d5eb9bd
Mark Perry <maperry78@yahoo.com.au>
2015-01-31 21:19:29 +1000

    Reviewed examples shown on the website

--- demo/src/main/java/fj/demo/Array_filter.java
+++ demo/src/main/java/fj/demo/Array_filter.java
@@ -7,10 +7,10 @@ import static fj.Show.intShow;
 import static fj.function.Integers.even;
 
 public final class Array_filter {
-  public static void main(final String[] args) {
-    final Array<Integer> a = array(97, 44, 67, 3, 22, 90, 1, 77, 98, 1078, 6, 64, 6, 79, 42);
-    final Array<Integer> b = a.filter(even);
-    final Array<Integer> c = a.filter(i -> i % 2 == 0);
-    arrayShow(intShow).println(b); // {44,22,90,98,1078,6,64,6,42}
-  }
+    public static void main(final String[] args) {
+        final Array<Integer> a = array(97, 44, 67, 3, 22, 90, 1, 77, 98, 1078, 6, 64, 6, 79, 42);
+        final Array<Integer> b = a.filter(even);
+        final Array<Integer> c = a.filter(i -> i % 2 == 0);
+        arrayShow(intShow).println(b); // {44,22,90,98,1078,6,64,6,42}
+    }
 }
--- demo/src/main/java/fj/demo/Option_bind.java
+++ demo/src/main/java/fj/demo/Option_bind.java
@@ -2,38 +2,40 @@ package fj.demo;
 
 import fj.F;
 import fj.data.Option;
-import static fj.data.Option.none;
-import static fj.data.Option.some;
 import static fj.Show.intShow;
 import static fj.Show.optionShow;
+import static fj.data.Option.none;
+import static fj.data.Option.some;
 
 public final class Option_bind {
-  public static void main(final String[] args) {
-    final Option<Integer> o1 = some(7);
-    final Option<Integer> o2 = some(8);
-    final Option<Integer> o3 = none();
-    final Option<Integer> p1 = o1.bind(new F<Integer, Option<Integer>>() {
-      public Option<Integer> f(final Integer i) {
-        return i % 2 == 0 ? some(i * 3) : Option.<Integer>none();
-      }
-    });
-    final Option<Integer> p2 = o2.bind(new F<Integer, Option<Integer>>() {
-      public Option<Integer> f(final Integer i) {
-        return i % 2 == 0 ? some(i * 3) : Option.<Integer>none();
-      }
-    });
-    final Option<Integer> p3 = o3.bind(new F<Integer, Option<Integer>>() {
-      public Option<Integer> f(final Integer i) {
-        return i % 2 == 0 ? some(i * 3) : Option.<Integer>none();
-      }
-    });
+    public static void main(final String[] args) {
+        final Option<Integer> o1 = some(7);
+        final Option<Integer> o2 = some(8);
+        final Option<Integer> o3 = none();
 
-    final Option<Integer> p4 = o1.bind(i -> i % 2 == 0 ? some(i * 3) : Option.<Integer>none());
-    final Option<Integer> p5 = o2.bind(i -> i % 2 == 0 ? some(i * 3) : Option.<Integer>none());
-    final Option<Integer> p6 = o3.bind(i -> i % 2 == 0 ? some(i * 3) : Option.<Integer>none());
+        F<Integer, Option<Integer>> f = i -> i % 2 == 0 ? some(i * 3) : none();
+        final Option<Integer> o4 = o1.bind(f);
+        final Option<Integer> o5 = o2.bind(f);
+        final Option<Integer> o6 = o3.bind(f);
 
-    optionShow(intShow).println(p1); // None
-    optionShow(intShow).println(p2); // Some(24)
-    optionShow(intShow).println(p3); // None
-  }
+        final Option<Integer> p1 = o1.bind(new F<Integer, Option<Integer>>() {
+            public Option<Integer> f(final Integer i) {
+                return i % 2 == 0 ? some(i * 3) : Option.<Integer>none();
+            }
+        });
+        final Option<Integer> p2 = o2.bind(new F<Integer, Option<Integer>>() {
+            public Option<Integer> f(final Integer i) {
+                return i % 2 == 0 ? some(i * 3) : Option.<Integer>none();
+            }
+        });
+        final Option<Integer> p3 = o3.bind(new F<Integer, Option<Integer>>() {
+            public Option<Integer> f(final Integer i) {
+                return i % 2 == 0 ? some(i * 3) : Option.<Integer>none();
+            }
+        });
+
+        optionShow(intShow).println(o4); // None
+        optionShow(intShow).println(o5); // Some(24)
+        optionShow(intShow).println(o6); // None
+    }
 }
