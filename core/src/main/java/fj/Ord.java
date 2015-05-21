1e6d9186d39f22ec2517a9cec2081af28aae0d9c
Dobes Vandermeer <dobesv@gmail.com>
2015-05-21 12:29:25 -0700

    Add "chain" combinator for Ord

--- core/src/main/java/fj/Ord.java
+++ core/src/main/java/fj/Ord.java
@@ -514,4 +514,21 @@ public final class Ord<A> {
   public Comparator<A> toComparator() {
 	  return new OrdComparator();
   }
+
+  /**
+   * Given two Ord instances that compare the given type of object, chain them together so
+   * that the result of the first is returned if it is not EQ, otherwise the result
+   * of the second is returned.  This can be used to reuse an Ord between subclasses
+   * or implementations of an interface.
+   *
+   * @return An Ord instance combining the two provided Ord instances
+   */
+  public static <A> Ord<A> chain(Ord<? super A> ord1, Ord<? super A> ord2) {
+	  return ord(a -> a2 -> {
+		  Ordering cmp = ord1.compare(a,  a2);
+		  if(cmp != Ordering.EQ)
+			  return cmp;
+		  return ord2.compare(a, a2);
+	  });
+  }
 }
