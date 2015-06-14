f2710c76525eda68f8d2fabbf1c1ac23316e141c
Mark Perry <maperry78@yahoo.com.au>
2015-06-14 23:53:18 +1000

    Added example comment for ch 7 of real world haskell

--- demo/src/main/java/fj/demo/realworld/Chapter7.java
+++ demo/src/main/java/fj/demo/realworld/Chapter7.java
@@ -11,6 +11,8 @@ import static fj.data.IOFunctions.*;
  * Created by MarkPerry on 11/06/2015.
  *
  * Examples from Chapter 7 of Real World Haskell, http://book.realworldhaskell.org/.
+ *
+ * Currently just ch07/toupper-lazy4.hs.
  */
 public class Chapter7 {
 
