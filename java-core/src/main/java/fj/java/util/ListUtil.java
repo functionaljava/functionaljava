4611c3524eca1786c112fcae0a727f2243b2d492
Mark Perry <maperry78@yahoo.com.au>
2015-08-28 01:50:18 +1000

    Implement filter, fold, bind for java.util.List

--- java-core/src/main/java/fj/java/util/ListUtil.java
+++ java-core/src/main/java/fj/java/util/ListUtil.java
@@ -1,6 +1,8 @@
 package fj.java.util;
 
 import fj.F;
+import fj.F2;
+
 import java.util.List;
 
 /**
@@ -11,4 +13,21 @@ public class ListUtil {
     public static <A, B> List<B> map(List<A> list, F<A, B> f) {
         return fj.data.List.list(list).map(f).toJavaList();
     }
+
+    public static<A> List<A> filter(List<A> list, F<A, Boolean> f) {
+        return fj.data.List.list(list).filter(f).toJavaList();
+    }
+
+    public static <A, B> B fold(List<A> list, F2<B, A, B> f, B b) {
+        return fj.data.List.list(list).foldLeft(f, b);
+    }
+
+    public static <A, B> List<B> flatMap(List<A> list, F<A, List<B>> f) {
+        return fj.data.List.list(list).bind(a -> fj.data.List.list(f.f(a))).toJavaList();
+    }
+
+    public static <A, B> List<B> bind(List<A> list, F<A, List<B>> f) {
+        return flatMap(list, f);
+    }
+
 }
