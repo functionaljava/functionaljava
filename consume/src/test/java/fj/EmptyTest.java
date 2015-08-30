cf074892be69f27bc0aa29068a4ad8db66d25d6b
Mark Perry <maperry78@yahoo.com.au>
2015-08-30 23:53:27 +1000

    Added integrated view of test coverage.  Turn retro lambda off temporarily

---
+++ consume/src/test/java/fj/EmptyTest.java
@@ -0,0 +1,18 @@
+package fj;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import org.junit.Assert;
+
+/**
+ * Created by MarkPerry on 30/08/2015.
+ */
+public class EmptyTest {
+
+    @Ignore @Test
+    public void missing() {
+        Assert.fail("not implemented");
+
+    }
+}
