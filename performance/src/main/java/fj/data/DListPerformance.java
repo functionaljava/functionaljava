3ce9d0ece1eea2f2ea9487050a5772a0df509c30
Mark Perry <maperry78@yahoo.com.au>
2016-02-06 09:45:56 +1000

    Small output changes to dlist peformance test

--- performance/src/main/java/fj/data/DListPerformance.java
+++ performance/src/main/java/fj/data/DListPerformance.java
@@ -5,16 +5,13 @@
  */
 package fj.data;
 
-import fj.data.DList;
-import fj.data.List;
-import fj.data.Seq;
-import fj.data.Stream;
-
 /**
  *
  * @author clintonselke
  */
 public class DListPerformance {
+
+    static final int TOTAL_RUNS = 10;
 
     private interface BenchmarkMethods<C> {
         C range(int from, int to);
@@ -40,7 +37,7 @@ public class DListPerformance {
     private static final BenchmarkMethods<Seq<Integer>> seqMethods = new BenchmarkMethods<Seq<Integer>>() {
         @Override
         public Seq<Integer> range(int from, int to) {
-            return Seq.seq(List.range(from, to));
+            return Seq.fromList(List.range(from, to));
         }
         @Override
         public Seq<Integer> append(Seq<Integer> a, Seq<Integer> b) {
@@ -69,7 +66,8 @@ public class DListPerformance {
 
     private static <C> double benchmark(BenchmarkMethods<C> methods) {
         long msStart = System.currentTimeMillis();
-        for (int n = 0; n < 10; ++n) {
+
+        for (int runNumber = 0; runNumber < TOTAL_RUNS; ++runNumber) {
             final C xs = methods.range(0, 100);
             C r = xs;
             for (int i = 1; i < 2000; ++i) {
@@ -79,19 +77,22 @@ public class DListPerformance {
             for (Integer x : r2) {}
         }
         long msEnd = System.currentTimeMillis();
-        return (msEnd - msStart) / 10.0;
+        return (msEnd - msStart) / ((double) TOTAL_RUNS);
     }
 
     public static void main(String[] params) {
+        System.out.println("Starting difference list (DList) performance benchmark...");
         // warm up
+        System.out.println("warm up...");
         benchmark(listMethods);
         benchmark(seqMethods);
         benchmark(dListMethods);
         // actual run
+        System.out.println("running benchmark...");
         double listTime = benchmark(listMethods);
         double seqTime = benchmark(seqMethods);
         double dListTime = benchmark(dListMethods);
-        System.out.println("Average over 10 runs...");
+        System.out.println("Average over " + TOTAL_RUNS + " runs...");
         System.out.println("List:  " + listTime + "ms");
         System.out.println("Seq:   " + seqTime + "ms");
         System.out.println("DList: " + dListTime + "ms");
