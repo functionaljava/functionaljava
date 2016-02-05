4eb7e9e4f3f5c8d1dcb905fe23c92f96300af325
Mark Perry <maperry78@yahoo.com.au>
2016-02-05 23:02:50 +1000

    Setup benchmarks module

---
+++ benchmarks/src/main/java/fj/data/dummy/DummyClass.java
@@ -0,0 +1,8 @@
+package fj.data.dummy;
+
+/**
+ * Created by MarkPerry on 5 Feb 16.
+ */
+public class DummyClass {
+
+}
---
+++ benchmarks/src/test/java/fj/data/dummy/DummyTest.java
@@ -0,0 +1,17 @@
+package fj.data.dummy;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Created by MarkPerry on 5 Feb 16.
+ */
+public class DummyTest {
+
+    @Test
+    @Ignore
+    public void test1() {
+
+    }
+
+}
