5f1d7ad6660745784987d6e219747ca8d1d8e5e4
Brian McKenna <bmckenna@atlassian.com>
2016-05-12 11:52:29 +1000

    Add property checks for Option optics

--- props-core-scalacheck/src/main/scala/fj/package.scala
+++ props-core-scalacheck/src/main/scala/fj/package.scala
@@ -1,5 +1,5 @@
 import fj.Equal
-import fj.data.List
+import fj.data.{Option, List}
 package object fj {
   implicit def Function1F[A, B](g: A => B): F[A, B] = new F[A, B] {
     def f(a: A) = g(a)
@@ -16,9 +16,11 @@ package object fj {
   implicit def intEqual: Equal[Int] = Equal.equal({(i1:Int, i2:Int) => Boolean.box((i1 == i2))})
 
   implicit def unitEqual: Equal[Unit] = Equal.anyEqual()
-  
+
   implicit def listEqual[A](implicit aEq: Equal[A]): Equal[List[A]] = Equal.listEqual(aEq)
-  
+
+  implicit def optionEqual[A](implicit aEq: Equal[A]): Equal[Option[A]] = Equal.optionEqual(aEq)
+
   implicit def p2Equal[A, B](implicit aEq: Equal[A], bEq: Equal[B]): Equal[P2[A, B]] = Equal.p2Equal(aEq, bEq)
   
 }
