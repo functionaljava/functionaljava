2a2b8eac96f2d2660987634fcf87394c0756b92c
Mark Perry <maperry78@yahoo.com.au>
2016-06-18 15:30:56 +1000

    Added priority queue properties

--- core/src/test/java/fj/data/PriorityQueueTest.java
+++ core/src/test/java/fj/data/PriorityQueueTest.java
@@ -14,42 +14,5 @@ import static org.junit.Assert.assertThat;
  */
 public class PriorityQueueTest {
 
-    @Test
-    public void test1() {
-        PriorityQueue<Integer, Integer> pq = PriorityQueue.<Integer>emptyInt();
-        PriorityQueue<Integer, Integer> pq2 = pq.enqueue(List.list(p(1, 1)));
-        out.println(pq2.toString());
-    }
-
-    @Test
-    public void empty() {
-        PriorityQueue<Integer, Integer> pq = PriorityQueue.<Integer>emptyInt();
-        assertThat(pq.isEmpty(), is(true));
-    }
-
-    @Test
-    public void top() {
-        PriorityQueue<Integer, Integer> pq1 = PriorityQueue.emptyInt();
-        Option<P2<Integer, Integer>> o = pq1.top();
-        assertThat(o, is(Option.none()));
-    }
-
-    @Test
-    public void dequeue() {
-        PriorityQueue<Integer, Integer> pq1 = PriorityQueue.<Integer>emptyInt().enqueue(List.list(
-                p(3, 6), p(10, 20), p(4, 8), p(1, 2)
-        ));
-        out.println(pq1);
-        assertThat(pq1.toString(), equalTo("PriorityQueue((3: 6), (10: 20), (4: 8), (1: 2))"));
-
-        PriorityQueue<Integer, Integer> pq2 = PriorityQueue.emptyInt();
-        PriorityQueue<Integer, Integer> pq3 = pq2.dequeue();
-        out.println(pq3);
-        assertThat(pq3.toString(), equalTo("PriorityQueue()"));
-
-        PriorityQueue<Integer, Integer> pq4 = pq1.dequeue();
-        out.println(pq4);
-        assertThat(pq4.toString(), equalTo("PriorityQueue((3: 6), (4: 8), (1: 2))"));
-    }
 
 }
---
+++ props-core/src/test/java/fj/data/properties/PriorityQueueProperties.java
@@ -0,0 +1,100 @@
+package fj.data.properties;
+
+import fj.Ord;
+import fj.P;
+import fj.P2;
+import fj.data.List;
+import fj.data.Option;
+import fj.data.PriorityQueue;
+import fj.data.Set;
+import fj.test.Arbitrary;
+import fj.test.Property;
+import fj.test.reflect.CheckParams;
+import fj.test.runner.PropertyTestRunner;
+import org.junit.runner.RunWith;
+
+import static fj.data.Option.some;
+import static fj.data.PriorityQueue.emptyInt;
+import static fj.test.Arbitrary.arbAlphaNumString;
+import static fj.test.Arbitrary.arbInteger;
+import static fj.test.Arbitrary.arbSet;
+import static fj.test.Arbitrary.arbitrary;
+import static fj.test.Property.impliesBoolean;
+import static fj.test.Property.prop;
+import static fj.test.Property.property;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by MarkPerry on 18 Jun 16.
+ */
+@RunWith(PropertyTestRunner.class)
+@CheckParams(maxSize = 100)
+public class PriorityQueueProperties {
+
+    public static Arbitrary<PriorityQueue<Integer, String>> arbPriorityQueueIntegerString = arbUniqueQueue(arbAlphaNumString);
+
+    public static <A> Arbitrary<PriorityQueue<Integer, A>> arbUniqueQueue(Arbitrary<A> aa) {
+        Arbitrary<Set<Integer>> as = arbSet(Ord.intOrd, arbInteger);
+        Arbitrary<List<Integer>> ints = arbitrary(as.gen.map(si -> si.toList()));
+        Arbitrary<List<P2<Integer, A>>> alp = arbitrary(
+                ints.gen.bind(li -> aa.gen.map(s -> li.map(i -> P.p(i, s))))
+        );
+        return arbitrary(alp.gen.map(l -> PriorityQueue.<A>emptyInt().enqueue(l)));
+    }
+
+    Property empty() {
+        PriorityQueue<Integer, Object> pq = emptyInt();
+        return prop(pq.isEmpty());
+    }
+
+    Property addRemove() {
+        return property(arbPriorityQueueIntegerString, arbInteger, arbAlphaNumString, (q, i, s) -> {
+            Option<P2<Integer, String>> o = q.top();
+            Option<P2<Integer, String>> o2 = q.enqueue(i, s).dequeue().top();
+            return Property.impliesBoolean(
+                    q.isGreaterThan(Ord.intOrd, i),
+//                    o.map(p -> i > p._1()).orSome(true),
+                    () -> o.equals(o2)
+            );
+        });
+    }
+
+    Property emptyTop() {
+        return prop(emptyInt().top().isNone());
+    }
+
+    Property addTop() {
+        return property(arbPriorityQueueIntegerString, arbInteger, arbAlphaNumString, (q, i, s) -> {
+            Option<P2<Integer, String>> actual = q.enqueue(i, s).top();
+            return impliesBoolean(
+                    q.isGreaterThan(Ord.intOrd, i),
+//                    q.top().map(p -> p._1() < i).orSome(true),
+                    actual.equals(some(P.p(i, s))));
+        });
+    }
+
+    Property sorted() {
+        Arbitrary<Set<Integer>> as = arbSet(Ord.intOrd, arbInteger);
+        Arbitrary<List<Integer>> ints = arbitrary(as.gen.map(si -> si.toList()));
+        Arbitrary<List<P2<Integer, String>>> alp = arbitrary(
+            ints.gen.bind(li -> arbAlphaNumString.gen.map(s -> li.map(i -> P.p(i, s))))
+        );
+        return property(alp, list -> {
+            PriorityQueue<Integer, String> q = PriorityQueue.<String>emptyInt().enqueue(list);
+            List<P2<Integer, String>> expected = list.sort(Ord.p2Ord1(Ord.intOrd.reverse()));
+            List<P2<Integer, String>> actual = q.toStream().toList();
+            assertThat(actual, equalTo(expected));
+            System.out.println(actual);
+            return prop(actual.equals(expected));
+        });
+    }
+
+    public Property sorted2() {
+        return property(arbPriorityQueueIntegerString, pq -> {
+            List<P2<Integer, String>> expected = pq.toList().sort(Ord.p2Ord1(Ord.intOrd.reverse()));
+            return prop(expected.equals(pq.toList()));
+        });
+    }
+
+}
