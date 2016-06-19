371831331cdc41486c0814f801e497ab12a26ed6
Mark Perry <maperry78@yahoo.com.au>
2016-06-19 23:58:40 +1000

    Added asserts, remove commented out and printlns from FingerTreeTest

--- props-core/src/test/java/fj/data/fingertrees/FingerTreeTest.java
+++ props-core/src/test/java/fj/data/fingertrees/FingerTreeTest.java
@@ -20,6 +20,8 @@ import static org.junit.Assert.assertThat;
  */
 public class FingerTreeTest {
 
+    public static final int SIZE = 10;
+
     @Test
     public void size() {
         validateSize(List.list(-92, 68, 54, -77, -18, 67));
@@ -36,20 +38,11 @@ public class FingerTreeTest {
 
     public FingerTree<Integer, Integer> midSeq() {
         FingerTree<Integer, Integer> ft = FingerTree.emptyIntAddition();
-        //        out.println(ft.toString());
-        return List.range(1, 10).foldLeft(ft2 -> i -> {
-            FingerTree<Integer, Integer> ft3 = ft2.snoc(i);
-//            out.println(ft3.toString());
-            return ft3;
-        }, ft);
+        return List.range(1, SIZE).foldLeft(ft2 -> i -> ft2.snoc(i), ft);
     }
-
-
 
     @Test
     public void testSeqString() {
-//        FingerTree<Integer, Integer> ft = FingerTree.emptyIntAddition();
-//        out.println(ft.toString());
         String actual = midSeq().toString();
         String expected = "Deep(9 -> One(1 -> 1), Deep(6 -> One(3 -> Node3(3 -> V3(2,3,4))), Empty(), One(3 -> Node3(3 -> V3(5,6,7)))), Two(2 -> V2(8,9)))";
         assertThat(actual, equalTo(expected));
@@ -57,47 +50,34 @@ public class FingerTreeTest {
 
     public FingerTree<Integer, P2<Integer, Integer>> midPriorityQueue() {
         FingerTree<Integer, P2<Integer, Integer>> ft = FingerTree.emptyIntMax();
-//        out.println(ft.toString());
-        return List.range(1, 10).foldLeft(ft2 -> i -> {
+        return List.range(1, SIZE).foldLeft(ft2 -> i -> {
             int j = i % 2 == 0 ? 2 * i : i;
             FingerTree<Integer, P2<Integer, Integer>> ft3 = ft2.snoc(P.p(j, j));
-//            out.println(ft3.toString());
             return ft3;
         }, ft);
     }
 
     @Test
     public void testQueueString() {
-//        FingerTree<Integer, P2<Integer, Integer>> ft = FingerTree.emptyIntMax();
-//        out.println(ft.toString());
         String actual = midPriorityQueue().toString();
         String expected = "Deep(16 -> One(1 -> (1,1)), Deep(12 -> One(8 -> Node3(8 -> V3((4,4),(3,3),(8,8)))), Empty(), One(12 -> Node3(12 -> V3((5,5),(12,12),(7,7))))), Two(16 -> V2((16,16),(9,9))))";
         assertThat(actual, equalTo(expected));
-
     }
 
     @Test
     public void stream() {
         FingerTree<Integer, Integer> ft = midSeq();
-        out.println(ft);
-        Stream<Integer> s = ft.toStream();
-        System.out.println(s.toList());
-
-        out.println(midPriorityQueue());
-        out.println(midPriorityQueue().toStream().toList());
+        assertThat(ft.toStream().toList(), equalTo(List.range(1, SIZE)));
     }
 
     @Test
     public void split() {
-
+        int splitPoint = 3;
         FingerTree<Integer, Integer> ft = FingerTree.emptyIntAddition();
-        FingerTree<Integer, Integer> ft3 = List.range(1, 10).foldLeft(ft2 -> i -> ft2.snoc(i), ft);
-        P2<FingerTree<Integer, Integer>, FingerTree<Integer, Integer>> p = ft3.split(v -> v >= 3);
-        Show<FingerTree<Integer, Integer>> fts = Show.fingerTreeShow(intShow, intShow);
-        Show<P2<FingerTree<Integer, Integer>, FingerTree<Integer, Integer>>> s = Show.p2Show(fts, fts);
-        System.out.println(s.showS(p));
-        System.out.println(p);
-
+        FingerTree<Integer, Integer> ft3 = List.range(1, SIZE).foldLeft(ft2 -> i -> ft2.snoc(i), ft);
+        P2<FingerTree<Integer, Integer>, FingerTree<Integer, Integer>> p = ft3.split(v -> v >= splitPoint);
+        assertThat(p._1().toStream().toList(), equalTo(List.range(1, splitPoint)));
+        assertThat(p._2().toStream().toList(), equalTo(List.range(splitPoint, SIZE)));
 
     }
 
