bfa703848f3c98e219a9e618ccf994fc57b455ba
Mark Perry <maperry78@yahoo.com.au>
2016-06-29 21:25:56 +1000

    Properties and ad hoc tests for HAMT properties

--- core/src/test/java/fj/data/hamt/HamtTest.java
+++ core/src/test/java/fj/data/hamt/HamtTest.java
@@ -3,9 +3,11 @@ package fj.data.hamt;
 import fj.Ord;
 import fj.P2;
 import fj.data.List;
+import fj.data.Option;
 import org.junit.Test;
 
 import static fj.Equal.intEqual;
+import static fj.Equal.optionEqual;
 import static fj.Hash.intHash;
 import static fj.P.p;
 import static fj.data.List.list;
@@ -13,86 +15,52 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 /**
- * Created by maperr on 3/06/2016.
+ * @author Mark Perry
  */
 public class HamtTest {
 
-    HashArrayMappedTrie<Integer, Integer> empty() {
-        return HashArrayMappedTrie.emptyKeyInteger();
+    public static final HashArrayMappedTrie<Integer, Integer> empty = HashArrayMappedTrie.emptyKeyInteger();
+
+    @Test
+    public void empty() {
+        assertThat(empty.length(), equalTo(0));
     }
 
     @Test
-    public void testEmpty() {
-        HashArrayMappedTrie<Integer, Integer> h = empty();
-//        out.println(h.toString());
-        assertThat(h.length(), equalTo(0));
+    public void lengthOne() {
+        assertThat(empty.set(3, 6).length(), equalTo(1));
     }
 
     @Test
-    public void one() {
-        HashArrayMappedTrie<Integer, Integer> h = empty().set(3, 6);
-//        out.println(h);
-        assertThat(h.length(), equalTo(1));
+    public void updateLength() {
+        HashArrayMappedTrie<Integer, Integer> h1 = empty.set(3, 3).set(3, 5);
+        assertThat(h1.length(), equalTo(1));
     }
 
     @Test
-    public void update() {
-        HashArrayMappedTrie<Integer, Integer> h1 = empty();
-        HashArrayMappedTrie<Integer, Integer> h2 = h1.set(3, 3);
-        HashArrayMappedTrie<Integer, Integer> h3 = h2.set(3, 5);
-//        out.println(h1);
-//        out.println(h2);
-//        out.println(h3);
-        assertThat(h3.length(), equalTo(1));
-    }
-
-    @Test
-    public void subtrieLength() {
+    public void streamLength() {
         List<P2<Integer, Integer>> list = list(p(0, 1), p(31, 1), p(32, 1), p(33, 1));
-        HashArrayMappedTrie<Integer, Integer> h = empty();
-        HashArrayMappedTrie<Integer, Integer> h2 = h.set(list);
-//        out.println(h);
-//        out.println(h2);
+        HashArrayMappedTrie<Integer, Integer> h2 = empty.set(list);
         assertThat(h2.toStream().length(), equalTo(list.length()));
     }
 
     @Test
-    public void setOverflow() {
-//        List<P2<Integer, Integer>> list = List.list(p(-17, 4), p(-16, 16), p(11, -11));
-//        List<P2<Integer, Integer>> list = List.list(p(11, -11));
-//        List<P2<Integer, Integer>> list = List.list(p(-17, 4), p(-16, 16), p(11, -11), p(16, -20), p(20, 14));
-
-        List<P2<Integer, Integer>> list = list(p(-5,2),p(-3,-6),p(0,-5),p(2,6),p(5,0));
-        HashArrayMappedTrie<Integer, Integer> e = HashArrayMappedTrie.<Integer, Integer>empty(intEqual, intHash);
-//        HashArrayMappedTrie<Integer, Integer> h1 = e.set(-5, -10);
-//        HashArrayMappedTrie<Integer, Integer> h2 = h1.set(-3, -6);
-        HashArrayMappedTrie<Integer, Integer> h2 = e.set(list);
-        List<P2<Integer, Integer>> actual = h2.toList(Ord.intOrd);
-        assertThat(list, equalTo(actual));
+    public void allIn() {
+        List<P2<Integer, Integer>> list = List.list(p(-5, 0), p(-1, -5), p(2, 4), p(4, -2));
+        HashArrayMappedTrie<Integer, Integer> h = empty.set(list);
+        Boolean b = list.foldLeft((acc, p) -> h.find(p._1()).option(false, i -> true && acc), true);
+        assertThat(b, equalTo(true));
     }
 
     @Test
-    public void overflow2() {
-//        java.lang.Error: Exception on property evaluation with argument:
-        List<P2<Integer, Integer>> list = list(p(-18, 9), p(-17, -7), p(-16, 9), p(-15, -14), p(-13, 9), p(-5, 11), p(-4, -6), p(1, -10), p(3, 15), p(6, 12), p(7, 16), p(16, 3));
-        HashArrayMappedTrie<Integer, Integer> e = HashArrayMappedTrie.<Integer>emptyKeyInteger();
-        HashArrayMappedTrie<Integer, Integer> h1 = e.set(list);
-
-
-
-    }
-
-    @Test
-    public void overflow3() {
-        overflowGen(list(p(6, 12), p(16, 3)));
-
-    }
-
-    public void overflowGen(List<P2<Integer, Integer>> list) {
-        HashArrayMappedTrie<Integer, Integer> e = HashArrayMappedTrie.<Integer>emptyKeyInteger();
-        HashArrayMappedTrie<Integer, Integer> h1 = e.set(list);
-        List<P2<Integer, Integer>> actual = h1.toList(Ord.intOrd);
-        assertThat(list, equalTo(actual));
+    public void sampleInts() {
+        List<P2<Integer, Integer>> ps = List.list(p(-3, 0), p(1, 2));
+        int key = -3;
+        HashArrayMappedTrie<Integer, Integer> h = empty.set(ps);
+        Option<Integer> o1 = ps.find(p -> intEqual.eq(p._1(), key)).map(p -> p._2());
+        Option<Integer> o2 = h.find(key);
+        boolean b = optionEqual(intEqual).eq(o1, o2);
+        assertThat(b, equalTo(true));
     }
 
 }
--- props-core/src/test/java/fj/data/hamt/HashArrayMappedTrieProperties.java
+++ props-core/src/test/java/fj/data/hamt/HashArrayMappedTrieProperties.java
@@ -5,6 +5,7 @@ import fj.Hash;
 import fj.Ord;
 import fj.P2;
 import fj.data.List;
+import fj.data.Option;
 import fj.data.Set;
 import fj.test.Gen;
 import fj.test.Property;
@@ -12,26 +13,28 @@ import fj.test.reflect.CheckParams;
 import fj.test.runner.PropertyTestRunner;
 import org.junit.runner.RunWith;
 
+import static fj.Equal.intEqual;
+import static fj.Equal.optionEqual;
 import static fj.Ord.intOrd;
 import static fj.Ord.p2Ord;
 import static fj.Ord.p2Ord2;
+import static fj.data.Option.some;
 import static fj.test.Arbitrary.arbInteger;
 import static fj.test.Arbitrary.arbList;
+import static fj.test.Arbitrary.arbListInteger;
 import static fj.test.Arbitrary.arbP2;
 import static fj.test.Arbitrary.arbSet;
 import static fj.test.Property.prop;
 import static fj.test.Property.property;
 
 /**
- * Created by MarkPerry on 25 Jun 16.
+ * @author Mark Perry
  */
 @RunWith(PropertyTestRunner.class)
 @CheckParams(maxSize = 100)
 public class HashArrayMappedTrieProperties {
 
-    private static final HashArrayMappedTrie<Integer, Integer> empty = HashArrayMappedTrie.empty(Equal.intEqual, Hash.intHash);
-//    private static final Gen<HashArrayMappedTrie<Integer, Integer>> arbHamt = arbHamt(Equal.intEqual, Hash.intHash, arbInteger, arbInteger);
-    private static final Ord<P2<Integer, Integer>> hamtOrd = p2Ord2(intOrd);
+    private static final HashArrayMappedTrie<Integer, Integer> empty = HashArrayMappedTrie.emptyKeyInteger();
     private static final Gen<List<P2<Integer, Integer>>> arbListProducts = arbSet(intOrd, arbInteger).bind(s -> Gen.listOf(arbInteger, s.size()).map(list -> s.toList().zip(list)));
     private static final Gen<HashArrayMappedTrie<Integer, Integer>> arbHamt = arbListProducts.map(l -> empty.set(l));
 
@@ -48,6 +51,44 @@ public class HashArrayMappedTrieProperties {
         });
     }
 
+    Property overwriteKey() {
+        return property(arbHamt, arbInteger, arbInteger, arbInteger, (h, k, v1, v2) -> {
+            Option<Integer> actual = h.set(k, v1).set(k, v2).find(k);
+            return prop(optionEqual(intEqual).eq(actual, some(v2)));
+        });
+    }
 
+    Property allIn() {
+        return property(arbListProducts, list -> {
+            HashArrayMappedTrie<Integer, Integer> h = empty.set(list);
+            Boolean b = list.foldLeft((acc, p) -> h.find(p._1()).option(false, i -> true && acc), true);
+            return prop(b);
+        });
+    }
+
+    Property sampleInts() {
+        return property(arbListProducts, arbInteger, (ps, i) -> {
+            HashArrayMappedTrie<Integer, Integer> h = empty.set(ps);
+            Option<Integer> o1 = ps.find(p -> intEqual.eq(p._1(), i)).map(p -> p._2());
+            Option<Integer> o2 = h.find(i);
+            return prop(optionEqual(intEqual).eq(o1, o2));
+        });
+    }
+
+    Property fold() {
+        return property(arbListProducts, list -> {
+            Integer actual = empty.set(list).foldLeft((acc, p) -> acc + p._2(), 0);
+            Integer expected = list.foldLeft((acc, p) -> acc + p._2(), 0);
+            return prop(intEqual.eq(actual, expected));
+        });
+    }
+
+    Property length() {
+        return property(arbListProducts, list -> {
+            Integer actual = empty.set(list).length();
+            Integer expected = list.length();
+            return prop(intEqual.eq(actual, expected));
+        });
+    }
 
 }
