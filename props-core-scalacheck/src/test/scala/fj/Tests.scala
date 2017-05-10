aa4de33faacb59c7dd24092fe3d3a65ca3a4c4d9
Ryan Johnson <johnson.1922@osu.edu>
2017-05-09 22:47:43 -0500

    Added working tests coverage for the StringBuffer and StringBuilder semigroup implementations.  Added the Semigroup tests to the list of Scalacheck test suite.

--- props-core-scalacheck/src/test/scala/fj/Tests.scala
+++ props-core-scalacheck/src/test/scala/fj/Tests.scala
@@ -1,8 +1,9 @@
 package fj
 
 object Tests {
-  def tests = List (
+  def tests = List(
     CheckP2.properties,
+    CheckSemigroup.properties,
     fj.data.CheckArray.properties,
     fj.data.CheckIO.properties,
     fj.data.CheckIteratee.properties,
@@ -19,26 +20,27 @@ object Tests {
 
   def main(args: Array[String]) {
     run(tests)
-//    System.exit(0)
+    //    System.exit(0)
   }
 
-  import org.scalacheck.Prop
-  import org.scalacheck.Test
   import org.scalacheck.Test.check
+  import org.scalacheck.{Prop, Test}
 
   def run(tests: List[(String, Prop)]) =
     tests foreach { case (name, p) => {
-        val c = check(new Test.Parameters.Default { override val maxSize = 20 }, p)
-        c.status match {
-          case Test.Passed => println("Passed " + name)
-          case Test.Proved(_) => println("Proved " + name)
-          case f @ Test.Failed(_, _) => sys.error(name + ": " + f)
-          case Test.Exhausted => println("Exhausted " + name)
-          case f @ Test.PropException(_, e, _) => {
-            e.printStackTrace
-            sys.error(name + ": " + f)
-          }
+      val c = check(new Test.Parameters.Default {
+        override val maxSize = 20
+      }, p)
+      c.status match {
+        case Test.Passed => println("Passed " + name)
+        case Test.Proved(_) => println("Proved " + name)
+        case f@Test.Failed(_, _) => sys.error(name + ": " + f)
+        case Test.Exhausted => println("Exhausted " + name)
+        case f@Test.PropException(_, e, _) => {
+          e.printStackTrace
+          sys.error(name + ": " + f)
         }
       }
+    }
     }
 }
