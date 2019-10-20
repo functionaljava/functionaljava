68828d3b8d72c8a481974902090f36b80e12bbd8
Yaroslav Atroshenko <yaaroslavok@gmail.com>
2019-10-20 18:49:01 +0300

    create tests for set monoid fabric methods

--- core/src/test/java/fj/MonoidTest.java
+++ core/src/test/java/fj/MonoidTest.java
@@ -1,6 +1,8 @@
 package fj;
 
+import fj.data.Enumerator;
 import fj.data.Option;
+import fj.data.Set;
 import fj.data.Stream;
 import org.junit.Test;
 
@@ -16,4 +18,26 @@ public class MonoidTest {
     assertThat(optionMonoid.sum(some(3), some(5)), is(some(8)));
     assertThat(optionMonoid.sumLeft(Stream.arrayStream(some(3), some(5))), is(some(8)));
   }
+
+  @Test
+  public void intersection_monoid_test() {
+    Bounded<Integer> integersBounded = Bounded.bounded(0, 10);
+    Monoid<Set<Integer>> intersectionMonoid = Monoid.setIntersectionMonoid(integersBounded, Enumerator.intEnumerator, Ord.intOrd);
+    Set<Integer> first = Set.set(Ord.intOrd, 1, 2, 3, 4);
+    Set<Integer> second = Set.set(Ord.intOrd, 3, 4, 5, 6);
+    Set<Integer> zero = intersectionMonoid.zero();
+    Set<Integer> actual = intersectionMonoid.sum(intersectionMonoid.sum(zero, first), second);
+    assertThat(actual, is(Set.set(Ord.intOrd, 3, 4)));
+  }
+
+  @Test
+  public void union_monoid_test() {
+    Monoid<Set<Integer>> intersectionMonoid = Monoid.setMonoid(Ord.intOrd);
+    Set<Integer> first = Set.set(Ord.intOrd, 1, 2, 3, 4);
+    Set<Integer> second = Set.set(Ord.intOrd, 3, 4, 5, 6);
+    Set<Integer> zero = intersectionMonoid.zero();
+    Set<Integer> actual = intersectionMonoid.sum(intersectionMonoid.sum(zero, first), second);
+    assertThat(actual, is(Set.set(Ord.intOrd, 1, 2, 3, 4, 5, 6)));
+  }
+
 }
