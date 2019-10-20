78b3f7600f102b2cee62e0a3a300b38131fc3b5e
Yaroslav Atroshenko <yaaroslavok@gmail.com>
2019-10-20 17:38:53 +0300

    implement bounded with factory methods

--- core/src/main/java/fj/Bounded.java
+++ core/src/main/java/fj/Bounded.java
@@ -2,9 +2,41 @@ package fj;
 
 public class Bounded<A> {
 
+    private final Definition<A> def;
+
     public interface Definition<A> {
         A min();
 
         A max();
+    }
+
+    private Bounded(Definition<A> definition) {
+        this.def = definition;
+    }
+
+    public A min() {
+        return def.min();
+    }
+
+    public A max() {
+        return def.max();
+    }
+
+    public static <A> Bounded<A> boundedDef(Definition<A> def) {
+        return new Bounded<>(def);
+    }
+
+    public static <A> Bounded<A> bounded(A min, A max) {
+        return boundedDef(new Definition<A>() {
+            @Override
+            public A min() {
+                return min;
+            }
+
+            @Override
+            public A max() {
+                return max;
+            }
+        });
     }
 }
