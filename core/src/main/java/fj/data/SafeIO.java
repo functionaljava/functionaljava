250aa82edd48edca4a315ee27f89cfc1bbb88e5b
Mark Perry <maperry78@yahoo.com.au>
2021-04-18 21:54:27 +1000

    Make SafeIO inherit of F0.

--- core/src/main/java/fj/data/SafeIO.java
+++ core/src/main/java/fj/data/SafeIO.java
@@ -1,13 +1,20 @@
 package fj.data;
 
+import fj.F0;
+
 /**
  * Created by MarkPerry on 3/07/2014.
  */
 @FunctionalInterface
-public interface SafeIO<A> extends IO<A> {
+public interface SafeIO<A> extends IO<A>, F0<A> {
 
 	@Override
 	A run();
+
+	@Override
+	default A f() {
+		return run();
+	}
 
 }
 
