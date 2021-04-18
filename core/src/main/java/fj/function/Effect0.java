2cad57e8c28f3f61dc929adf1387b0a48a1c354b
Mark Perry <maperry78@yahoo.com.au>
2021-04-18 21:56:05 +1000

    Added conversions from Effect0 to Try, TryEffect and F0.

--- core/src/main/java/fj/function/Effect0.java
+++ core/src/main/java/fj/function/Effect0.java
@@ -1,6 +1,8 @@
 package fj.function;
 
-import fj.F0;
+import fj.*;
+
+import static fj.Unit.unit;
 
 /**
  * Created by mperry on 28/08/2014.
@@ -8,5 +10,30 @@ import fj.F0;
 public interface Effect0 {
 
 	void f();
+
+	default F0<Unit> toF0() {
+		return () -> {
+			f();
+			return unit();
+		};
+	}
+
+	default <E extends Exception> TryEffect0<E> toTryEffect0() {
+		return () -> f();
+	}
+
+	default <E extends Exception> Try0<Unit, E> toTry0() {
+		return () -> {
+			f();
+			return unit();
+		};
+	}
+
+	default P1<Unit> toP1() {
+		return P.lazy(() -> {
+			f();
+			return unit();
+		});
+	}
 
 }
